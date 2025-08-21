4dd52a0066adb7e94d822c722852072c2deab5d9
circlefour <0100.circle@gmail.com>
2025-08-20 22:15:53 -0400

    added unix word list to this dir, and accessing it from file, maybe i will make it more general, get words from a db instead of a file idk, but dope start

--- search_string.py
+++ search_string.py
@@ -1,10 +1,12 @@
 import random
 
 class SearchStringGenerator:
-    def __init__(self, word_list, min_words=1, max_words=3):
-        self.word_list = word_list
+    def __init__(self, word_list='words.txt', min_words=1, max_words=3):
         self.min_words = min_words
         self.max_words = max_words
+
+        with open(word_list) as f:
+            self.word_list = [line.rstrip('\n') for line in f if line.strip()]
 
     def get_rand_word(self):
         return random.choice(self.word_list)
